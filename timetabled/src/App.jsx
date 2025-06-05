3b8797d1b0b4c66fa6da0e435965b4edd85f09b4
Ammar Khalil <ammarkhalil786@gmail.com>
2025-06-04 21:02:44 -0400

    Lab 1: Created basic React Calender in team!

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calender from './components/calender';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Timetabled</h1>
+      <h2>...</h2>
+      <Calender/>
+    </div>
+  )
+}
+
+export default App
---
+++ timetabled/src/components/Calender.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import Event from "./Event";
+
+const Calender = () => {
+    return (
+        <div className="Calender">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 a.m.</td>
+                        <Event event='Fancy Dinner 🎩' color ='green'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Yolk 🍳' color ='green'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 a.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Subway 🚊' color ='pink'/>
+                        <td></td>
+                        <td></td>
+                        <Event event='The Bean 🫘' color ='blue'/>
+                    </tr>
+                    <tr>
+                        <td className="time">10 a.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 a.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 p.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 p.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 p.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 p.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 p.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 p.m.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Calender;
